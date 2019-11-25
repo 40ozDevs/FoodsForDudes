a98d2cfd79d9dc87908abbd5f5762599d97c4c94
root <root@DESKTOP-U3Q6B83.localdomain>
2019-11-25 16:03:17 -0600

    testing online repo with minor change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br />
-          hi mom.
+          bottom text
         </p>
         <a
           className="App-link"
