c1bd01765549cac713333852691c4c6555424a6a
Alex Villanueva <37161837+AlexVanilla@users.noreply.github.com>
2019-12-14 12:19:56 -0800

    adjust layout

--- src/App.js
+++ src/App.js
@@ -10,20 +10,26 @@ function App() {
   return (
     <Container className="">
       
-      <Row className="">
+      <Row className="mt-10">
         <Col className = "d-flex justify-content-end hamburger-btn">
           <div className="hamburger-bar"></div>
           <div className="hamburger-bar"></div>
           <div className="hamburger-bar"></div>
         </Col>
       </Row>
-      <Row className="">
-        < Col className = "justify-content-center">
+      <Row className="mt-10">
+        <Col className = "d-flex justify-content-end hamburger-btn">
+          <h1 className="text-center">Foods4Dudes</h1>
+          <p className="text-center">It's in the name</p>
+        </Col>
+      </Row>
+      <Row className="mt-100" mt={100}>
+        <Col xs={{ span: 6, offset: 3}} className="justify-content-center" md="auto">
           <InputGroup className="">
             <FormControl id="recipe-search" />
           </InputGroup>
         </Col>
-        <Col className="">
+        <Col className="" md="auto">
           <Button>Search</Button>
         </Col>
       </Row> 
